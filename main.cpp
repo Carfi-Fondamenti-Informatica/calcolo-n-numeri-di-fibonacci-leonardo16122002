86835fb440d625b444a2db4a8b5020d2b68c2350
leonardo16122002 <78797793+leonardo16122002@users.noreply.github.com>
2021-11-09 16:57:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-   return 0;
+    __int64 n,i=0,j=1,k,l;
+    cin>>n;
+    if(n>1){
+        cout<<1<<endl;
+        for(l=0;l<n-1;l++){
+            k=i+j;
+            cout<<k<<endl;
+            i=j;
+            j=k;
+        }
+    }else{
+        cout<<"errore"<<endl;
+    }
 }
